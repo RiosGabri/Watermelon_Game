7b4576ed498ba6c61e0bc5b4506cb190cf52377f
Gabriel Parméra <grp@cesar.school>
2026-05-03 10:23:40 -0300

    fix: Corrigi erros na estrutura e centralizei a lógica no arquivo fruits.h
    Ainda está em fase de prototipação

--- src/fruits.h
+++ src/fruits.h
@@ -1,12 +1,13 @@
-#ifndef __FRUIT_H__ 
-#define __FRUIT_H__
+#ifndef __FRUITS_H__ 
+#define __FRUITS_H__
 
 #include "chipmunk/chipmunk.h"
 #include "raylib.h"
 
 #define MAX_FRUITS     128
-#define FRUIT_LEVELS   8
+#define NIVEIS_FRUTA   8
 
+//Nesse espaço deve possuir as dimensões do jogo (janela)
 
 typedef struct Def_Fruta{
     float  raio;   
@@ -17,8 +18,8 @@ typedef struct Def_Fruta{
 extern const FruitaDef DEFS_FRUTA[NIVEIS_FRUTA];
  
 typedef struct {
-    cpBody  *body;      
-    cpShape *shape;     
+    cpBody  *body;      /*Posição e velocidade do objeto rígido*/
+    cpShape *shape;     /*Colisão*/  
     int      nivel;     /* Nível da fruta*/
     int      ativa;     /* 1 = existe, 0 = removida*/
     int      fundindo;  /* flag temporário de fusão*/
