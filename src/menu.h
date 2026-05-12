4320bfac3a8f845db270b98edbed6e967fc6e800
Gabriel Parméra <grp@cesar.school>
2026-05-12 15:22:18 -0300

    refactor: created menu.h

--- src/menu.h
+++ src/menu.h
@@ -1,24 +1,15 @@
-#ifndef __MENU_H__
-#define __MENU_H__
- 
+#ifndef MENU_H
+#define MENU_H
+
 #include "raylib.h"
 
-typedef enum menu{
-    Menu_Aguardando,
-    Menu_Jogar,
-    Menu_Sair
-} Estado_Menu;
+typedef enum jogo {
+    EST_MENU,
+    EST_JOGO,
+    EST_CONFIG
+} Estado_Jogo;
 
-typedef enum painel{
-    Painel_Nenhum,
-    Painel_Configuracoes,
-    Painel_Musicas
-} Painel_Ativo;
-
-typedef struct configuracoes{
-    float Volume_Musica;     
-    int   Largura_Janela;
-    int   Altura_Janela;
-} Configuracoes;
+void desenha_botao(Rectangle ret, const char *texto, Color cor_base, Color cor_hover);
+int foi_clicado(Rectangle ret);
 
 #endif
