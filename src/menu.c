f7006db6d3f7574f8b7feeeeb7e3425271b962e0
Gabriel Parméra <grp@cesar.school>
2026-05-13 12:41:06 -0300

    Update menu.c para acompanhar as mudanças

--- src/menu.c
+++ src/menu.c
@@ -1,27 +1,20 @@
 #include "menu.h"
 
-#define RGB(r, g, b) (Color){r, g, b, 255}
-
-void desenha_botao(Botao b)
-{
-    Vector2 mouse = GetMousePosition();
-
-    bool hover = CheckCollisionPointRec(mouse, b.area);
-
-    Texture2D textura = hover
-        ? b.hover
-        : b.normal;
-
-    DrawTexture(
-        textura,
-        (int)b.area.x,
-        (int)b.area.y,
-        WHITE
-    );
+bool foi_clicado(Botao b) {
+    return CheckCollisionPointRec(GetMousePosition(), b.area) &&
+           IsMouseButtonPressed(MOUSE_LEFT_BUTTON);
 }
 
-bool foi_clicado(Botao b)
-{
-    return CheckCollisionPointRec(GetMousePosition(), b.area)
-        && IsMouseButtonPressed(MOUSE_LEFT_BUTTON);
+void desenha_botao(Botao b) {
+    bool hover = CheckCollisionPointRec(GetMousePosition(), b.area);
+    Color cor = hover ? LIGHTGRAY : WHITE;
+
+    DrawTexturePro(
+        b.textura,
+        (Rectangle){0, 0, (float)b.textura.width, (float)b.textura.height},
+        b.area,
+        (Vector2){0, 0},
+        0.0f,
+        cor
+    );
 }
